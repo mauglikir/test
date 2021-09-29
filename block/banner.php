5ba827f0a75585ddeff2b33bed5e7b8cbc21aa34
mauglikir <62102203+mauglikir@users.noreply.github.com>
2021-09-29 11:31:20 +0300

    dsdsc343d

--- block/banner.php
+++ block/banner.php
@@ -3,7 +3,7 @@
             <p>Welcome to Help Center!</p>
             <form ... class="aui main-banner-form">
                 <div class="field-group">
-                    <input class="text long-field" placeholder="Gopa" type="text" id="text-input" name="text-input" title="Text input">
+                    <input class="text long-field" placeholder="How can we halp you? " type="text" id="text-input" name="text-input" title="Text input">
                     <div class="buttons"><span class="aui-icon aui-icon-small aui-iconfont-search">Insert meaningful text here for accessibility</span></div>
                 
             </form>
